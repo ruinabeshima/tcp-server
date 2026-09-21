d5fd860fae79ee533bbe9b36def36a96e3e42145
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-09-21 13:57:23 +0900

    feat: open port 8080 on machine and listen for connections

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main 
 
-import "fmt"
+import (
+	"net"
+	"fmt"
+)
 
 func main(){
-	fmt.Print("Hello World")
+
+	// Open port 8080 on machine and listen for connections 
+	listener, err = net.Listen("tcp", "8080")
+	// Error handling
+	if err != "nil" {
+		fmt.Println("Error starting server: ", err)
+		os.Exit(1)
+	}
+	// defer: delays execution until surrounding function finishes
+	// Close connection when main() finishes 
+	defer listener.Close()
+
+	fmt.Println("TCP server is listening on port 8080...")
+
 }
